53a2e98b2d975f9dea2d8a35fc7b64d7fb5a846b
Henry Omino <henryedwin92@gmail.com>
2025-10-01 17:33:32 +0300

    addition of button component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,16 @@
 //pages/about.tsx
+import Button from "@/components/common/Button";
 
-export default function AboutPage () {
-    return(
-        <h1>About Us</h1>
-    );
+export default function AboutPage() {
+  return (
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold mb-6">About Us</h1>
+
+      <div className="flex gap-4">
+        <Button label="Small Rounded" size="small" shape="rounded-sm" />
+        <Button label="Medium Rounded" size="medium" shape="rounded-md" />
+        <Button label="Large Circle" size="large" shape="rounded-full" />
+      </div>
+    </div>
+  );
 }
